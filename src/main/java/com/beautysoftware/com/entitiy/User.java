8c70a599141b5bd001571a94aa144649c9c1bb28
Rubenvaldo desktop <rubo.rfn@gmail.com>
2020-11-21 13:52:28 +0000

    saturday meeting test
    saturday meeting test

--- src/main/java/com/beautysoftware/com/entitiy/User.java
+++ src/main/java/com/beautysoftware/com/entitiy/User.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class // some funcitionality has been implemented
-public class User { // making some change changing again
+public class User { // making some change changing again another change
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
